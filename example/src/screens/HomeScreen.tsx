745484ad293db46c1e43cec7d73ae470f1be9484
tientv <tientv20@fpt.com.vn>
2021-07-05 17:36:00 +0700

    chore: control groups

--- example/src/screens/HomeScreen.tsx
+++ example/src/screens/HomeScreen.tsx
@@ -1,4 +1,3 @@
-import { useFocusEffect } from '@react-navigation/native';
 import type { StackScreenProps } from '@react-navigation/stack';
 import type { FC } from 'react';
 import React from 'react';
@@ -9,6 +8,11 @@ import nameof from 'ts-nameof.macro';
 import NodeView from '../components/NodeView';
 import { DeviceControlScreen } from './DeviceControlScreen';
 import DeviceScanningScreen from './DeviceScanningScreen';
+import PagerView from 'react-native-pager-view';
+import type { Group } from '../models/group';
+import { asyncStorageRepository } from '../repositories/async-storage-repository';
+import groupList from '../data/groups.json';
+import GroupView from '../components/GroupView';
 
 export const HomeScreen: FC<Partial<StackScreenProps<any>>> = (
   props: Partial<StackScreenProps<any>>
@@ -27,6 +31,25 @@ export const HomeScreen: FC<Partial<StackScreenProps<any>>> = (
   React.useEffect(() => {
     TelinkBle.autoConnect();
   }, [navigation]);
+
+  const [groups, setGroups] = React.useState<Group[]>([]);
+
+  React.useEffect(() => {
+    const unsubscribe = navigation?.addListener('focus', async () => {
+      const groupAsync = await asyncStorageRepository.getGroups();
+      if (groupAsync.length !== 0) {
+        setGroups(groupAsync);
+      } else {
+        await asyncStorageRepository.saveGroups(groupList as Group[]);
+        setGroups(await asyncStorageRepository.getGroups());
+      }
+    });
+    return function cleanup() {
+      if (unsubscribe) {
+        unsubscribe();
+      }
+    };
+  }, [navigation, props.navigation]);
 
   const handleNavigateToDeviceControl = React.useCallback(
     (node: NodeInfo) => () => {
@@ -69,86 +92,82 @@ export const HomeScreen: FC<Partial<StackScreenProps<any>>> = (
   }, []);
 
   return (
-    <FlatList
-      style={styles.outerContainer}
-      contentContainerStyle={styles.innerContainer}
-      data={nodes}
-      keyExtractor={(nodeInfo: NodeInfo) => nodeInfo.address}
-      renderItem={({ item, index }) => {
-        return (
-          <>
-            <NodeView
-              node={item}
-              index={index}
-              onPress={() => {
-                handleNavigateToDeviceControl(item)();
-              }}
-            />
-          </>
-        );
-      }}
-      ListHeaderComponent={
-        <>
-          <FAB
-            style={styles.fab}
-            small
-            icon="plus"
-            onPress={() => {
-              navigation?.navigate(DeviceScanningScreen.displayName!);
+    <>
+      <PagerView
+        style={styles.pagerView}
+        initialPage={0}
+        showPageIndicator={true}
+        transitionStyle={'curl'}
+      >
+        <View key="1">
+          <FlatList
+            style={styles.outerContainer}
+            contentContainerStyle={styles.innerContainer}
+            data={nodes}
+            keyExtractor={(nodeInfo: NodeInfo) => nodeInfo.address}
+            renderItem={({ item, index }) => {
+              return (
+                <>
+                  <NodeView
+                    node={item}
+                    index={index}
+                    onPress={handleNavigateToDeviceControl(item)}
+                  />
+                </>
+              );
+            }}
+            ListHeaderComponent={
+              <>
+                <FAB
+                  style={styles.fab}
+                  small
+                  icon="plus"
+                  onPress={() => {
+                    navigation?.navigate(DeviceScanningScreen.displayName!);
+                  }}
+                />
+                <View style={styles.actions}>
+                  <Button
+                    onPress={() => {
+                      TelinkBle.getNodes().then((newNodes: NodeInfo[]) => {
+                        setNodes(newNodes);
+                        console.log(newNodes);
+                      });
+                    }}
+                  >
+                    Get nodes
+                  </Button>
+                  <Button
+                    onPress={() => {
+                      TelinkBle.setAllOn();
+                    }}
+                  >
+                    All on
+                  </Button>
+                  <Button
+                    onPress={() => {
+                      TelinkBle.setAllOff();
+                    }}
+                  >
+                    All off
+                  </Button>
+                </View>
+              </>
+            }
+          />
+        </View>
+        <View key="2">
+          <FlatList
+            style={styles.flatListView}
+            data={groups}
+            keyExtractor={(group: Group) => group.id.toString()}
+            renderItem={({ item, index }) => {
+              return <GroupView group={item} key={index} />;
             }}
           />
-          <View style={styles.actions}>
-            <Button
-              onPress={() => {
-                TelinkBle.getNodes().then((newNodes: NodeInfo[]) => {
-                  setNodes(newNodes);
-                  console.log(newNodes);
-                });
-              }}
-            >
-              Get nodes
-            </Button>
-            <Button
-              onPress={() => {
-                TelinkBle.resetBle();
-              }}
-            >
-              Reset nodes
-            </Button>
-          </View>
-          <View style={styles.actions}>
-            <Button
-              onPress={() => {
-                TelinkBle.setAllOn();
-              }}
-            >
-              All on
-            </Button>
-            <Button
-              onPress={() => {
-                TelinkBle.setAllOff();
-              }}
-            >
-              All off
-            </Button>
-          </View>
-          <Snackbar
-            visible={visible}
-            onDismiss={() => {
-              setVisible(false);
-            }}
-            action={{
-              label: 'Dismiss',
-              onPress: () => {
-                setVisible(false);
-              },
-            }}
-          >
-            {meshConnectionMessage}
-          </Snackbar>
-        </>
-      }
-    />
+        </View>
+      </PagerView>
+    </>
   );
 };
 
@@ -177,5 +196,16 @@ const styles = StyleSheet.create({
   },
   actions: {
     flexDirection: 'row',
+  },
+  pagerView: {
+    flex: 1,
+  },
+  itemView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 16,
+  },
+  flatListView: {
+    padding: 16,
   },
 });
