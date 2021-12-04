087b12969cd441146a095ad07e4fe54e01273d15
thanhtunguet <thanhtung.uet@gmail.com>
2021-12-04 22:52:19 +0700

    feat: add test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,7 @@
-it.todo('write a test');
+import { uint16ToHexString } from '../helpers/native';
+
+describe('native functions', () => {
+  it('to short', () => {
+    expect(uint16ToHexString(0xffff)).toEqual('ff ff');
+  });
+});
